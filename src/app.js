18c4ca2699e3675c7487b0e9fdc34957200148e3
Charles Ellis <hamled@hamled.name>
2016-11-28 08:50:58 -0800

    Structure for doing market simulation w/ events

--- src/app.js
+++ src/app.js
@@ -1,5 +1,21 @@
 import $ from 'jquery';
+import _ from 'underscore';
 import ApplicationView from 'app/views/application_view';
+
+const simulate = function(quote) {
+  // Calculate a random price movement
+  const maxChange = 1.00;
+  const minChange = 0.00;
+  var change = _.random(minChange * 10, maxChange * 10) / 10;
+
+  // Decide if the change is positive or negative
+  if(_.random(0,1) === 1) {
+    change *= -1;
+  }
+
+  // Actually trigger the change
+  quote.trigger('change:price', change);
+};
 
 $(document).ready(function() {
   var appView = new ApplicationView({
@@ -7,4 +23,8 @@ $(document).ready(function() {
   });
 
   appView.render();
+
+  setInterval(function() {
+    // Call simulate() on each quote in the ApplicationView
+  }, 1000);
 });
