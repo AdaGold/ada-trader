bc575f11185a3828c5e1010dd3876ba302e7b7c9
Charles Ellis <hamled@hamled.name>
2016-11-28 07:11:20 -0800

    Remove Application model to simplify

--- src/app.js
+++ src/app.js
@@ -1,13 +1,9 @@
 import $ from 'jquery';
-import Application from 'app/models/application';
 import ApplicationView from 'app/views/application_view';
 
 $(document).ready(function() {
-  var application = new Application();
-
   var appView = new ApplicationView({
-    el: '#mount',
-    model: application
+    el: '#application'
   });
 
   appView.render();
