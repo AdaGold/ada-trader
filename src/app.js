c978b9eede23adcc2b1b4fa3998ed9f45034919f
Charles Ellis <hamled@hamled.name>
2017-12-09 18:17:48 -0800

    Remove ApplicationView

--- src/app.js
+++ src/app.js
@@ -3,16 +3,9 @@ import './css/app.css';
 
 import $ from 'jquery';
 import _ from 'underscore';
-import ApplicationView from './views/application_view';
 import Simulator from './models/simulator';
 
 $(document).ready(function() {
-  const appView = new ApplicationView({
-    el: '#application'
-  });
-
-  appView.render();
-
   const simulator = new Simulator({
 //  quotes: <quote collection instance>
   });
