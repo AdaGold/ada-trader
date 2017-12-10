3a1b5645d640e69ef51fadb228ec2319f87241ae
Charles Ellis <hamled@hamled.name>
2017-12-09 18:29:16 -0800

    Tests for Quote model

---
+++ spec/models/quote_spec.js
@@ -0,0 +1,31 @@
+import Quote from 'models/quote';
+
+describe('Quote spec', () => {
+  let quote;
+  beforeEach(() => {
+    quote = new Quote({
+      symbol: 'HELLO',
+      price: 100.00,
+    });
+  });
+
+  describe('Buy function', () => {
+    it('increases the price by $1.00', () => {
+      const startPrice = quote.get('price');
+
+      quote.buy();
+
+      expect(quote.get('price')).toEqual(startPrice + 1.00);
+    });
+  });
+
+  describe('Sell function', () => {
+    it('decreases the price by $1.00', () => {
+      const startPrice = quote.get('price');
+
+      quote.sell();
+
+      expect(quote.get('price')).toEqual(startPrice - 1.00);
+    });
+  });
+});
