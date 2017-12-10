72cdeb5f985f70d862bc29c288d0f2373c43d8a2
Charles Ellis <hamled@hamled.name>
2017-12-09 20:22:41 -0800

    Provide static quote data & connect to simulator

--- src/app.js
+++ src/app.js
@@ -3,11 +3,33 @@ import './css/app.css';
 
 import $ from 'jquery';
 import _ from 'underscore';
-import Simulator from './models/simulator';
+
+import Simulator from 'models/simulator';
+import QuoteList from 'collections/quote_list';
+
+const quoteData = [
+  {
+    symbol: 'HUMOR',
+    price: 88.50,
+  },
+  {
+    symbol: 'CLOTH',
+    price: 81.70,
+  },
+  {
+    symbol: 'HABIT',
+    price: 98.00,
+  },
+  {
+    symbol: 'SUPER',
+    price: 83.10,
+  },
+];
 
 $(document).ready(function() {
+  const quotes = new QuoteList(quoteData);
   const simulator = new Simulator({
-//  quotes: <quote collection instance>
+    quotes: quotes,
   });
 
   simulator.start();
